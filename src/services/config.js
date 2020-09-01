f3ca1bba579c34c94147b976056ee4336e8ced55
Gaharana <mr.priyanshgaharana98@gmail.com>
2020-09-01 09:44:58 +0530

    fixed  apirurl

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
-const apiUrl = 'http://localhost:4000';
-// const apiUrl = 'https://immense-hamlet-02246.herokuapp.com'
+// const apiUrl = 'http://localhost:4000';
+const apiUrl = 'https://immense-hamlet-02246.herokuapp.com'
 const config = {
   apiUrl,
 };
