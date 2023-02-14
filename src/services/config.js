32e4be44db428fad3a63d86bfe9462752864f83d
Priyansh Gaharana <gaharanap@google.com>
2023-02-14 20:35:18 +0530

    Change base url to app engine

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,8 @@
 // const apiUrl = 'http://localhost:4000';
 const apiUrl =
-  process.env.NODE_ENV === 'production' ? 'https://poaa-api.poaa.tk' : 'http://localhost:4000';
+  process.env.NODE_ENV === 'production'
+    ? 'https://seedbox-299206.el.r.appspot.com'
+    : 'http://localhost:4000';
 const config = {
   apiUrl,
 };
