522217ce62f57525fd74e818b3682ba1519a6ae6
Priyansh Gaharana <gaharanap@google.com>
2023-02-15 00:12:39 +0530

    Chagne base url

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 // const apiUrl = 'http://localhost:4000';
 const apiUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://seedbox-299206.el.r.appspot.com'
+    ? 'https://poaa-api.onrender.com'
     : 'http://localhost:4000';
 const config = {
   apiUrl,
