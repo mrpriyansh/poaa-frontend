f1db7a76441d6f18955704b6785f9710f21f521a
mrpriyansh <mr.priyanshgaharana@gmail.com>
2023-02-28 11:29:37 +0530

    Update base url

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,8 @@
 // const apiUrl = 'http://localhost:4000';
 const apiUrl =
-  process.env.NODE_ENV === 'production' ? 'https://poaa-api.poaa.tk' : 'http://localhost:4000';
+  process.env.NODE_ENV === 'production'
+    ? 'https://poaa-api-r7g3efabnq-nn.a.run.app'
+    : 'http://localhost:4000';
 const config = {
   apiUrl,
 };
