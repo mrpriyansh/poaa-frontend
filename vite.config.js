9456abeecaf5eb8cd23bffd18d4b2a92b8964b1d
mrpriyansh <mr.priyanshgaharana@gmail.com>
2023-05-01 21:10:08 +0530

    Add vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import vitePluginRequire from 'vite-plugin-require';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: 'build',
     },
-    plugins: [react(), svgr(), vitePluginRequire()],
+    plugins: [react(), svgr(), vitePluginRequire(), VitePWA({ registerType: 'autoUpdate' })],
   };
 });
