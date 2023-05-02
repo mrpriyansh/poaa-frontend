5b900533d70d719caa65c3c549bfe7cbd907125a
mrpriyansh <mr.priyanshgaharana@gmail.com>
2023-05-02 10:48:46 +0530

    Add control to unregister pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import vitePluginRequire from 'vite-plugin-require';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
   return {
     build: {
       outDir: 'build',
@@ -16,6 +18,7 @@ export default defineConfig(() => {
       VitePWA({
         registerType: 'autoUpdate',
         devOptions: { enabled: true },
+        selfDestroying: Boolean(env.VITE_UNREGISTER_SW),
       }),
     ],
   };
